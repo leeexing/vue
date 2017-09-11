0f74024c70b2e7651259ecdd5acba99c8bdd71f9
leeexing <674023427@qq.com>
2017-09-11 18:34:48 +0800

    add one NSTS mission

---
+++ vue-koa/src/components/admin/Music/music.vue
@@ -0,0 +1,130 @@
+<template>
+  <div id="music">
+    <div class="search-content">
+      <h3 class="title">用户列表</h3>
+      <p class="small">
+        <i class="el-icon-edit"></i>
+        一些提示
+      </p>
+      <div class="search-bar">
+        <i class="el-icon-search"></i>
+        <input type="text" id="search" placeholder="请输入搜索用户名">
+      </div>
+      <ul class="sortable scroll">
+        <li>
+          悲伤坚决不放手
+          开始纠缠之后
+          才又被人放大了自由
+          <div class="detail">
+            <i class="el-icon-time"></i>
+            2017-09-11
+            <a href="#" class="btn check">查看</a>
+          </div>
+        </li>
+        <li>
+          对面 湖水对蓝天信纸对笔尖 小伤疤对创可贴 你在我对面
+          星辰对山巅 蝴蝶对枕边 一场梦对一双眼 我在你对面
+          啊 像宇宙的浩瀚无际无边 像平行线上没有过交点
+          <div class="detail">
+            <i class="el-icon-time"></i>
+            2017-09-12
+            <a href="#" class="btn check">查看</a>
+          </div>
+        </li>
+        <li>
+          若是归来去: 吸引我的不仅仅是天后的嗓音，还有亘古不变的自拍封面
+          <div class="detail">
+            <i class="el-icon-time"></i>
+            2017-09-12
+            <a href="#" class="btn check">查看</a>
+          </div>
+        </li>
+        <li>
+          若是归来去: 吸引我的不仅仅是天后的嗓音，还有亘古不变的自拍封面
+          <div class="detail">
+            <i class="el-icon-time"></i>
+            2017-09-12
+            <a href="#" class="btn check">查看</a>
+          </div>
+        </li>
+        <li>
+          若是归来去: 吸引我的不仅仅是天后的嗓音，还有亘古不变的自拍封面
+          <div class="detail">
+            <i class="el-icon-time"></i>
+            2017-09-12
+            <a href="#" class="btn check">查看</a>
+          </div>
+        </li>
+        <li>
+          若是归来去: 吸引我的不仅仅是天后的嗓音，还有亘古不变的自拍封面
+          <div class="detail">
+            <i class="el-icon-time"></i>
+            2017-09-12
+            <a href="#" class="btn check">查看</a>
+          </div>
+        </li>
+        <li>
+          若是归来去: 吸引我的不仅仅是天后的嗓音，还有亘古不变的自拍封面
+          <div class="detail">
+            <i class="el-icon-time"></i>
+            2017-09-13
+            <a href="#" class="btn check">查看</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="show-content">
+      <div class="show-box">
+        <div class="title">
+          <h1>这里是用户信息的标题</h1>
+        </div>
+        <div class="info-content">
+          <div class="timeline">
+            <div class="timeline-icon">
+              <i class="el-icon-information"></i>
+            </div>
+            <div class="timeline-content">
+              <h2>周一</h2>
+              <p>家驹的歌哪个能模仿，模仿也只能模仿，无法超越。
+                紫棋不是模仿，是改编，是有紫棋自己的味道，很好听。
+                欣赏她的才华与实力，加油</p>
+              <span class="sub-info">
+                <i class="el-icon-time"></i>
+                2017/9/99
+              </span>
+              <a href="#" class="btn-info">更多</a>
+            </div>
+          </div>
+          <div class="timeline">
+            <div class="timeline-icon">
+              <i class="el-icon-star-on"></i>
+            </div>
+            <div class="timeline-content">
+              <h2>周一</h2>
+              <p>家驹的歌哪个能模仿，模仿也只能模仿，无法超越。
+                紫棋不是模仿，是改编，是有紫棋自己的味道，很好听。
+                欣赏她的才华与实力，加油</p>
+              <span class="sub-info">
+                <i class="el-icon-time"></i>
+                2017/9/99
+              </span>
+              <a href="#" class="btn-info">更多</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import './music.less'
+export default {
+  name: 'Music'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
