60bd3d07973a7db8f29c410e377d44221a8fa3e1
Leekinm <leejingyaha@163.com>
2018-01-07 21:41:13 +0800

    add a discover page

--- vue-nuxt/pages/index.vue
+++ vue-nuxt/pages/index.vue
@@ -1,21 +1,5 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        vue-nuxt
-      </h1>
-      <h2 class="subtitle">
-        learn ssr in order to reset project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-        <a href="/about" class="button--grey">about</a>
-        <a href="/blog" class="button--grey">blog</a>
-      </div>
-    </div>
-  </section>
+  <logo/> 
 </template>
 
 <script>
